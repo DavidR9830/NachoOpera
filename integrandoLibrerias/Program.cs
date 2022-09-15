9550df7e874dd3d0998dfb2b442777dca8190d29
Pablo <zapatagallegojuan@gmail.com>
2022-09-14 21:35:48 -0500

    integracion de la Libreria Suma

--- integrandoLibrerias/Program.cs
+++ integrandoLibrerias/Program.cs
@@ -1,6 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using libreriaSuma;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Ingrese el primer numero");
 int num1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Ingrese el segundo numero");
 int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("La suma de los 2 primeros numeros es " + Class1.Suma(num1, num2));
 
