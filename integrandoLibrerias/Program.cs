6bc248b499ed759bef327b2695531ebe5cca0d8c
Pablo <zapatagallegojuan@gmail.com>
2022-09-14 21:05:23 -0500

    Agregar archivos de proyecto.

---
+++ integrandoLibrerias/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese el primer numero");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero");
+int num2 = int.Parse(Console.ReadLine());
+
